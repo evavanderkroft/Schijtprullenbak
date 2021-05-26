0a67e185e36383b6ed64e05864e0223fa72d75e5
NaudR366 <57040315+NaudR366@users.noreply.github.com>
2021-05-26 12:17:15 +0200

    servo

---
+++ arduino.js
@@ -0,0 +1,10 @@
+function rotateOne(){
+    const url = 'http://localhost:3000/rotateOne';
+    fetch(url)
+    .then(console.log("rotate 90"))
+}
+function rotateTwo(){
+    const url = 'http://localhost:3000/rotateTwo';
+    fetch(url)
+    .then(console.log("Rotate 0"))
+}
---
+++ servo.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors())
+
+const {Board, Servo} = require("johnny-five");
+const board = new Board();
+
+
+
+app.get('/rotateOne', (req, res) => {
+    res.send("Rotate 90!")
+    servoOne()
+})
+
+app.get('/rotateTwo', (req, res) => {
+    res.send("Rotate 0!")
+    servoTwo()
+})
+
+app.listen(3000, () => {
+    console.log("Server has started and is listening on port 3000")
+})
+
+board.on("ready", () => {
+  const servo = new Servo(10);
+
+  // Add servo to REPL (optional)
+  board.repl.inject({
+    servo
+  });
+
+
+
+servoOne = () => {
+    servo.to(180);
+}
+
+servoTwo = () => {    
+    servo.to(90);
+}
+  
+
+
+//   servo.sweep();
+});
